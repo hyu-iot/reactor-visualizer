e0cdf82d0e9849976e00480dec1ad6202ffc5c9b
CloverCho <snsc7878@gmail.com>
2022-04-13 13:50:04 +0900

    Add diagram visualization

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
-const app = require('express')();
+const express = require('express');
+express.static.mime.define({'application/wasm': ['wasm']})
+
+const app = express();
 const server = require('http').createServer(app);
 const io = require("socket.io")(server, {
     cors: {
